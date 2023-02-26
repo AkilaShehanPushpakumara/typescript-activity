9acb47024451809ef9a4d714c92f9bd7668d55b8
AkilaShehanPushpakumara <109887376+AkilaShehanPushpakumara@users.noreply.github.com>
2023-02-26 19:30:30 +0530

    Add files via upload

---
+++ test.ts
@@ -0,0 +1,50 @@
+import{t} from 'testcafe'
+import{Selector} from 'testcafe'
+import { faker } from '@faker-js/faker';
+
+const firstName = faker.name.firstName(); 
+const lastName = faker.name.lastName(); 
+const zipCode = faker.address.zipCode();
+
+fixture('Get start')
+.page('https://www.saucedemo.com')
+
+.beforeEach(async t => {
+    await t.setTestSpeed(0.1)
+  })
+
+test('first',async t =>{
+await t
+
+.typeText('#user-name','performance_glitch_user')
+.typeText('#password','secret_sauce')
+
+.click('#login-button')
+
+.expect(Selector('.inventory_item_name').withText('Sauce Labs Fleece Jacket').exists)
+    .ok()
+
+.expect(Selector('.inventory_item_price').withText('49.99').exists)
+.ok()
+
+.click('#add-to-cart-sauce-labs-backpack')
+.click('#add-to-cart-sauce-labs-bike-light')
+
+.click('#shopping_cart_container')
+
+.expect(Selector('#item_4_title_link,#item_0_title_link').exists).ok()
+
+.click('#checkout')
+
+.typeText('#first-name', firstName)
+.typeText('#last-name', lastName)
+.typeText('#postal-code', zipCode)
+
+
+.click('#continue')
+
+
+.click('#finish')
+
+
+});
